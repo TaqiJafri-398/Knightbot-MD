2714cb19ca90d9a656f0acbe4a169be4f8e62bdb
TaqiJafri-398 <149189544+TaqiJafri-398@users.noreply.github.com>
2025-02-09 09:21:25 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Hacker Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Taqi Jafri', // Your name
   ownerNumber: '994402779705', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
